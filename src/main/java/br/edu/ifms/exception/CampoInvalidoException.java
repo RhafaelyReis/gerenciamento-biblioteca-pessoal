d067fc67bdf7f45141f1274efb79ca8dc2af0507
Rhafaely Ananda Costa dos Reis <rhafaelyreis@gmail.com>
2025-07-18 00:25:16 -0400

    🩹 patch: corrige mensagem redundante de erro

--- src/main/java/br/edu/ifms/exception/CampoInvalidoException.java
+++ src/main/java/br/edu/ifms/exception/CampoInvalidoException.java
@@ -3,6 +3,6 @@ package br.edu.ifms.exception;
 public class CampoInvalidoException extends Exception{
     @Override
     public String getMessage() {
-        return "Erro: o valor informado em um dos campos é inválido. Verifique se o ano é válido!";
+        return "O valor informado em um dos campos é inválido. Verifique se o ano é válido!";
     }
 }
